7ba9720146b328c611a500309489e808b7a50a4a
Karl Moran <karlmoran06@gmail.com>
2022-01-29 12:56:34 +0000

    Added Two functions to javaScript randomBox and moveMole

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,3 +1,21 @@
-const Boxes = document.getElementsByClassName('.box');
-const Mole = document.getElementsByClassName('.mole');
-const Timer = document.getElementById('#timer');
+const boxes = document.getElementsByClassName('.box');
+const mole = document.getElementsByClassName('.mole');
+const timer = document.getElementById('#timer');
+
+let yourScore = 0
+
+function randomBox(){
+    boxes.forEach(box => {
+        box.classList.remove('.mole')
+        
+    })
+
+    let randomBox = boxes[Math.floor(math.random() * 12)]
+    randomBox.classList.add('.mole')
+
+}
+
+function moveMole(){
+    let timerId = null
+    timerId = setInterval(randomBox,800)
+}
