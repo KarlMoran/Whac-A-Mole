7a1a613b8e5ec90f6a353830f8844b7c8285f889
Karl Moran <karlmoran06@gmail.com>
2022-01-29 14:16:32 +0000

    Tried to fix randomBox

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,21 +1,26 @@
-const boxes = document.getElementsByClassName('.box');
-const mole = document.getElementsByClassName('.mole');
-const timer = document.getElementById('#timer');
+/** 
+ * declare constants for DOM elements 
+ * and possible choice 
+ */
 
-let yourScore = 0
+const boxes = document.getElementsByClassName("box");
+const mole = document.getElementsById("mole");
+const timer = document.getElementById("timer");
+const score = document.getElementById("score");
+
+let yourScore = 0;
+
 
 function randomBox(){
     boxes.forEach(box => {
-        box.classList.remove('.mole')
-        
-    })
+     box.classList.remove("mole");
+    });
 
-    let randomBox = boxes[Math.floor(math.random() * 12)]
-    randomBox.classList.add('.mole')
-
+    let randomBox = boxes[Math.floor(Math.random() * 12)];
+    randomBox.classList.add("mole");
 }
 
 function moveMole(){
-    let timerId = null
-    timerId = setInterval(randomBox,800)
+    let timerId = null;
+    timerId = setInterval(randomBox,500);
 }
